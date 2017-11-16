bfdae0826ac58010b7cb90082be8b4e1a9708deb
chenkaiyao <chenkaiyao@molmc.com>
2017-11-16 09:22:00 +0800

    初始版本

---
+++ src/geometry_msgs.h
@@ -0,0 +1,69 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2011, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *  * Neither the name of Willow Garage, Inc. nor the names of its
+ *    contributors may be used to endorse or promote prducts derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#ifndef _GEOMETRY_MSGS_H
+#define _GEOMETRY_MSGS_H
+
+#include "Accel.h"
+#include "AccelStamped.h"
+#include "AccelWithCovariance.h"
+#include "AccelWithCovarianceStamped.h"
+#include "Inertia.h"
+#include "InertiaStamped.h"
+#include "Point.h"
+#include "Point32.h"
+#include "PointStamped.h"
+#include "Polygon.h"
+#include "PolygonStamped.h"
+#include "Pose.h"
+#include "Pose2D.h"
+#include "PoseArray.h"
+#include "PoseStamped.h"
+#include "PoseWithCovariance.h"
+#include "PoseWithCovarianceStamped.h"
+#include "Quaternion.h"
+#include "QuaternionStamped.h"
+#include "Transform.h"
+#include "TransformStamped.h"
+#include "Twist.h"
+#include "TwistStamped.h"
+#include "TwistWithCovariance.h"
+#include "TwistWithCovarianceStamped.h"
+#include "Vector3.h"
+#include "Vector3Stamped.h"
+#include "Wrench.h"
+#include "WrenchStamped.h"
+
+#endif
